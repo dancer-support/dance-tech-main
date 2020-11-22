5c32209604d76e6b00d0aa7ef84c01bb74e55d7c
Yuma Sumi <rbly.776324.yuma66krayt@gmail.com>
2020-11-22 20:52:37 +0900

    removed param

--- store/actions.js
+++ store/actions.js
@@ -18,12 +18,12 @@ export default {
     if (ctx.res && ctx.res.locals && ctx.res.locals.user) {
       const { allClaims: claims, ...authUser } = ctx.res.locals.user
 
-      console.info(
-        'Auth User verified on server-side. User: ',
-        authUser,
-        'Claims:',
-        claims
-      )
+      // console.info(
+      //   'Auth User verified on server-side. User: ',
+      //   authUser,
+      //   'Claims:',
+      //   claims
+      // )
 
       await dispatch('onAuthStateChanged', {
         authUser,
@@ -40,7 +40,7 @@ export default {
     commit('SET_AUTH_USER', { authUser })
   },
 
-  checkVuexStore (ctx) {
+  checkVuexStore () {
     if (this.$fire.auth === null) {
       throw new Error('Vuex Store example not working - this.$fire.auth cannot be accessed.')
     }
