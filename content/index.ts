6d1746f2f284abae740a5fa5d83600e37f52a7ef
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-10-12 22:35:53 +0900

    fix bug

--- content/index.ts
+++ content/index.ts
@@ -1,5 +1,5 @@
 import * as contentful
-export client = contentful.createClient({
-  space: proses.env.CONTENTFUL_SPACE_ID
+export const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
 })
