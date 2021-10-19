fa6daadc8895ce236a6569aff9c6784d5541b042
daigotanaka0714 <daig.tanaka0714@gmail.com>
2021-10-19 21:59:01 +0900

    content内に使ってないfileがあったので削除

--- content/index.ts
+++
@@ -1,5 +0,0 @@
-import * as contentful from 'contentful'
-export const client = contentful.createClient({
-  space: process.env.CONTENTFUL_SPACE_ID as string,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string
-})
