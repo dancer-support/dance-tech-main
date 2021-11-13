33808692ffc9c2e6f65563a9181d822e6565c3dd
daigotanaka0714 <daig.tanaka0714@gmail.com>
2021-11-13 16:25:53 +0900

    add dummyapi to make responsive layout

--- client/pages/performance/index.vue
+++ client/pages/performance/index.vue
@@ -1,20 +1,31 @@
 <template>
   <client-only>
     <masonry
-      :cols="{ default: 3, 1500: 4, 1000: 2, 700: 1, 400: 1 }"
-      :gutter="{ default: '30px', 700: '15px' }"
+      :cols="{ default: 6, 1920: 4, 1200: 3, 1024: 3, 768: 3, 480: 3 }"
+      :gutter="{
+        default: '10px',
+        1920: '10px',
+        1200: '10px',
+        1024: '10px',
+        768: '6px',
+        480: '5px',
+      }"
     >
       <div v-for="(item, index) in items" :key="index">
         <v-img
           rel="preload"
           as="image"
           class="white--text align-end"
-          :lazy-src="item.url"
+          :src="item.image"
           gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
           max-width="300"
           contain
         >
-          <v-card-title v-text="item.title" />
+          <v-card-subtitle v-text="item.text" />
+          <v-icon class="ml-2 mb-2">
+            mdi-heart
+          </v-icon>
+          <span class="subheading ml-2 mb-2">{{ item.likes }}</span>
         </v-img>
       </div>
     </masonry>
@@ -25,16 +36,14 @@
 export default {
   name: 'PagePerformance',
   async asyncData ({ $axios }) {
-    const photos = await $axios.$get(
-      'https://jsonplaceholder.typicode.com/photos',
-      {
-        params: {
-          _limit: 20
-        }
+    const photos = await $axios.$get('https://dummyapi.io/data/v1/post', {
+      params: { limit: 50 },
+      headers: {
+        'app-id': process.env.DUMMY_APP_ID
       }
-    )
+    })
     return {
-      items: photos
+      items: photos.data
     }
   }
 }
