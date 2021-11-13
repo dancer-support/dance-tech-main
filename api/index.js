4865c31a8f721964813770381bf04873059ceed8
daigotanaka0714 <daig.tanaka0714@gmail.com>
2021-11-13 13:10:09 +0900

    fix lint

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get("/", function(req, res) {
-  res.send("HelloWorld");
-});
+app.get('/', function (req, res) {
+  res.send(`HelloWorld${req.params}`)
+})
 
 module.exports = {
-  path: "/api/",
+  path: '/api/',
   handler: app
-};
+}
