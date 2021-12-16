fe802050751e3a880bb45010a83aff0f83dabd63
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-12-16 20:58:29 +0900

    #273 PORT環境変数が3000だった場合は4545に変更する

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,14 @@
 import './pre-start'; // Must be the first import
 import app from '@server';
 import logger from '@shared/Logger';
+require('dotenv').config();
 
+if (process.env.PORT === '3000') {
+  process.env.PORT = '4545'
+}
 
 // Start the server
-const port = Number(4000);
+const port = Number(process.env.PORT);
 app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
 });
