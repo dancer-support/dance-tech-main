bcf0ca667a5cb8c0f5a4852bed8b1d7de53b06d9
Yuma Sumi <rbly.776324.yuma66krayt@gmail.com>
2022-02-17 21:40:04 +0900

    made backend to headless

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,5 +1,9 @@
-import { Router } from 'express';
+import { Router } from "express";
+import * as Users from "./Users";
 
 // Export the base-router
 const baseRouter = Router();
+
+baseRouter.use("/users/all", Users.getAllUsers);
+
 export default baseRouter;
