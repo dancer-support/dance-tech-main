e0558177844eab1a411ff3c4a693c50ba1440fa2
daigotanaka0714 <daigo.tanaka0714@gmail.com>
2022-03-31 21:29:28 +0900

    wip add performance index pages

---
+++ client/components/atom/icon/location.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="d-flex d-inline-flex">
+    <v-icon class="location-icon" aria-label="location" role="img" aria-hidden="false" size="12">
+      {{ location }}
+    </v-icon>
+    <div class="location-name text-caption">
+      {{ locationName }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mdiMapMarker } from '@mdi/js'
+export default {
+  name: 'Location',
+  props: {
+    locationName: {
+      type: String,
+      default: '東京'
+    }
+  },
+  data () {
+    return {
+      location: mdiMapMarker
+    }
+  }
+}
+</script>
+
+<style scoped>
+.location-icon {
+  margin: 0.5rem;
+}
+.location-name {
+  margin: 0.25rem;
+  color: #3E3E3E;
+}
+</style>
