bc06cf86f8cecdc909babba795aae2f2b99dd02b
Yuma Sumi <rbly.776324.yuma66krayt@gmail.com>
2022-04-13 23:55:58 +0900

    displayed dancers

--- client/pages/performances/_id.vue
+++ client/pages/performances/_id.vue
@@ -4,12 +4,30 @@
       back
     </NuxtLink>
     <h1>{{ performance.title }}</h1>
-    <div>
+    <section>
       <!-- FIXME: WideImageから外部の画像を表示できていない模様なので、一時的にアイコンを表示している -->
       <!-- <AtomImageWideImage :src="performance.image_url" /> -->
       <AtomImageWideImage :src="link" />
       <p>{{ performance.description }}</p>
-    </div>
+    </section>
+    <section>
+      <h2>Dancers</h2>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="i in 3"
+            :key="i"
+            sm="4"
+            no-gutters
+          >
+            <v-card v-if="i <= performance.dancers.length" class="pa-2">
+              <AtomImageWideImage :src="link" />
+              <p>{{ names[i - 1] }}</p>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
   </div>
 </template>
 
@@ -19,7 +37,15 @@ export default ({
     const id = params.id
     const { performance } = await $axios.$get(`/performances/${id}`)
     const link = '/profile.png'
-    return { performance, link }
+    const names = performance.dancers.map((dancer) => {
+      const { first_name_en: firstName, last_name_en: lastName } = dancer
+      return `${firstName} ${lastName}`
+    })
+    return {
+      performance,
+      link,
+      names
+    }
   }
 })
 </script>
